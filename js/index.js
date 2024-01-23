66a39ef9392daf87a7aa683a35ef2e41e77e65d6
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-01-23 11:19:03 +0700

    js update

--- js/index.js
+++ js/index.js
@@ -5,3 +5,27 @@ const navBtn = document.querySelector("#nav-btn");
 navBtn.addEventListener("click", () => {
   navBar.classList.toggle("show-nav-bar");
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', revealOnScroll);
+function revealOnScroll() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
